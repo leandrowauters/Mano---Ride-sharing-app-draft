29279afa9111e162ec0ceca07534ffae2b41922d
John Doe <leandro.wauters@gmail.com>
2019-08-15 16:37:07 -0400

    Before driverRideCompleted

---
+++ Mano/Controllers/Driver/DriverRideCompletedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DriverRideCompletedViewController.swift
+//  Mano
+//
+//  Created by Leandro Wauters on 8/15/19.
+//  Copyright © 2019 Leandro Wauters. All rights reserved.
+//
+
+import UIKit
+
+class DriverRideCompletedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
